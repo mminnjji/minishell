53a02142e8bd6a4152245e0dda648aa0ab89cbd3
mminnjji <man@student.42seoul.kr>
2023-11-16 16:13:16 +0900

    파이프 마지막 문제

--- parse.c
+++ parse.c
@@ -1,42 +1,45 @@
 #include "minishell.h"
 
-// 파이프 기준으로 연결리스트 할당
-int get_malloc_list(char *str, t_cmd *start)
+int check_quote_pipe(char *str, char c) // 파이프와 따옴표 확인
 {
     int i;
-    int count;
     int flag[2];
-    t_cmd *tmp;
+    int count;
 
     i = -1;
     count = 0;
     flag[0] = 0;
     flag[1] = 0;
-    tmp = start;
     while (str[++i])
     {
-        if (str[i] == '\"')
-            flag[0] = 1 - flag[0];
-        if (str[i] == '\'')
-            flag[1] = 1 - flag[1];
-        if (str[i] == '|' && flag[0] == 0 && flag[1] == 0) //따옴표 안이 아닐 경우
+        if (str[i] == '\"' || str[i] == '\'')
+            flag[str[i] % 2] = 1 - flag[str[i] % 2];
+        if (str[i] == c && flag[0] == 0 && flag[1] == 0) //따옴표 안이 아닐 경우
         {
             count++;
             while (str[i + 1] == ' ')
                 i++;
-            if (str[i + 1] == '|') // 연속된 파이프가 등장할 경우
-            {
-                printf("syntax error\n");
+            if (c == '|' && str[i + 1] == c) // 연속된 파이프가 등장할 경우
                 return (-1);
-            }
         }
         if (flag[0] * flag[1] == 1) // 따옴표 안 닫혔는데 .. 새로 다른 따옴표 열렸을 경우
-        {
-            printf("syntax error\n");
             return (-1);
-        }
     }
     if (flag[0] == 1 || flag[1] == 1) // 따옴표 안닫혔을 경우
+        return (-1);
+    return (count);
+}
+
+// 파이프 기준으로 연결리스트 할당
+int get_malloc_list(char *str, t_cmd *start)
+{
+    int i;
+    int count;
+    t_cmd *tmp;
+
+    tmp = start;
+    count = check_quote_pipe(str, '|');
+    if (count < 0)
     {
         printf("syntax error\n");
         return (-1);
@@ -53,84 +56,48 @@ int get_malloc_list(char *str, t_cmd *start)
     return (count);
 }
 
-int parse_by_pipe(int pip[], char *str, t_cmd *start, int pipe_n)
+int parse_by_pipe(int pip[], char *str, t_cmd **start, int pipe_n)
 {
     int i;
     t_cmd *tmp;
-    t_arg *des;
 
     i = 0;
-    tmp = start;
-    while (start && i < pipe_n + 1)
+    tmp = (*start);
+    while (tmp && i < pipe_n + 1)
     {
-        des = start->arg;
-        des = malloc(sizeof(t_arg));
-        if (!des)
-            return (1); // 이경우에 이전에 할당된 구조체 연결리스트 + 구조체 참조 구조체 arg + arg str 모두 해제해줘야함
-        des->init_cmd = malloc(sizeof(char) * (pip[i + 1] - pip[i] + 2));
-        if (!des->init_cmd)
+        tmp->arg = malloc(sizeof(t_arg));
+        if (!tmp->arg)
+            return (1); // 이경우에 이전에 할당된 구조체 연결리스트 + else 해제
+        tmp->arg->init_cmd = malloc(sizeof(char) * (pip[i + 1] - pip[i] + 2));
+        if (!tmp->arg->init_cmd)
             return (1); // 동일
-        ft_strlcpy(des->init_cmd, (const char *)(str + pip[i]), pip[i + 1] - pip[i] + 1);
-        if (des->init_cmd[strlen(des->init_cmd) - 1] == '|')
-            des->init_cmd[strlen(des->init_cmd) - 1] = 0;
-        printf("str : %s pipe0: %d pipe1: %d\n", des->init_cmd, pip[i], pip[i + 1]);
-        start = start->next;
+        ft_strlcpy(tmp->arg->init_cmd, (const char *)(str + pip[i]), pip[i + 1] - pip[i] + 1);
+        if (tmp->arg->init_cmd[strlen(tmp->arg->init_cmd) - 1] == '|')
+            tmp->arg->init_cmd[strlen(tmp->arg->init_cmd) - 1] = 0;
+        tmp = tmp->next;
         i++;
     }
-    start = tmp;
+    while (*start)
+    {
+        printf("cmd : %s\n", (*start)->arg->init_cmd);
+        (*start) = (*start)->next;
+    }
     return (0);
-}
-
-static int	get_line(char const *str, char c)
-{
-	int	i;
-	int	count;
-    int flag;
-
-	count = 0;
-	i = 0;
-    flag = 0;
-	while (str[i])
-	{
-		while (str[i] == c && flag == 0)
-			i++;
-		if (str[i] != c && str[i])
-		{
-			count++;
-			while ((str[i] != c && str[i]) || flag == 1)
-            {
-                if (str[i] == '"')
-                i++;
-            }
-		}
-	}
-	return (count);
 }
 
 int parse_by_space(t_cmd *start)
 {
     t_arg *des;
-    int flag[2];
     int i;
 
     i = 0;
-    flag[0] = 0;
-    flag[1] = 0;
     des = start->arg;
-    des->parse = (char **)malloc((get_line(des->init_cmd, ' ') + 1) * sizeof(char *));
-    while (des->init_cmd[i])
+    des->parse = (char **)malloc((check_quote_pipe(des->init_cmd, ' ') + 2) * sizeof(char *));
+    printf(" 덩어리: %d \n" ,check_quote_pipe(des->init_cmd, ' ') + 2);
+    /*while (des->init_cmd[i])
     {
-        if (des->init_cmd[i] == '\"')
-            flag[0] = 1 - flag[0];
-        if (des->init_cmd[i] == '\'')
-            flag[1] = 1 - flag[1];
-        if (des->init_cmd[i] == ' ')
-        {
-            while (des->init_cmd[i] == ' ')
-                i++;
-        }
-        i++;
-    }
+
+    }*/
     return (0);
 }
 
@@ -157,7 +124,8 @@ int get_init_cmd_list(char *str, t_cmd *start, int pipe_n)
             pip[++j] = i + 1;
     }
     pip[pipe_n + 1] = i;
-    if (parse_by_pipe(pip, str, start, pipe_n))
+    if (parse_by_pipe(pip, str, &start, pipe_n))
         return (1);
+    //parse_by_space(start);
     return (0);
 }
